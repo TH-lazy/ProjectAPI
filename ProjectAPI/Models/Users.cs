2d0827bff957f3fab810b4229521241d2674da0a
TH-lazy <luuminhhieu01082000@gmail.com>
2023-05-27 00:11:25 +0700

    Commit Create User Model

---
+++ ProjectAPI/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectAPI.Models
+{
+    public class Users
+    {
+        public string UserID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public DateTime LastVisited { get; set; }
+        public int Active { get; set; }
+        public string ShopID { get; set; }
+
+    }
+}
